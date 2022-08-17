e56d4326aeb7867427ce3bdb578fe29adc808b2a
Nuno Filipe Costa Frutuoso <nfilipe-@c1r16s8.42lisboa.com>
2022-08-17 05:58:58 +0100

    C02_ex09

--- c_02/ft_strcpy.c
+++ c_02/ft_strcpy.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
 char	*ft_strcpy(char *dest, char *src)
 {
 	char	*lenght;
@@ -19,41 +17,9 @@ char	*ft_strcpy(char *dest, char *src)
 	lenght = 0;
 	while (*lenght != '\0')
 	{
-		dest = src;
-		lenght++;
-		return (dest);
+		dest[i] = src[i];
+		i++;
 	}
-	return (src);
+	dest[i] = '\0';
+	return (dest);
 }
-
-/*
-man strcpy
-
-char chararray[6];
-
-	     (void)strncpy(chararray, "abc", sizeof(chararray));
-
-     The following sets chararray to ``abcdef'':
-
-	     char chararray[6];
-
-	     (void)strncpy(chararray, "abcdefgh", sizeof(chararray));
-
-     Note that it does not NUL terminate chararray because the length of the source
-     string is greater than or equal to the length argument.
-
-     The following copies as many characters from input to buf as will fit and NUL termi-
-     nates the result.  Because strncpy() does not guarantee to NUL terminate the string
-     itself, this must be done explicitly.
-
-	     char buf[1024];
-
-	     (void)strncpy(buf, input, sizeof(buf) - 1);
-	     buf[sizeof(buf) - 1] = '\0';
-
-     This could be better achieved using strlcpy(3), as shown in the following example:
-
-	     (void)strlcpy(buf, input, sizeof(buf));
-
-	     
-*/
